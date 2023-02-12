542466734cf8efb003770c3cf6b6e6e228e7582e
Alva Jufinto <yeahbruh199@gmail.com>
2023-02-12 21:03:20 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,9 @@
+const App = () => {
+    return (
+      <div>
+        React App with CDN
+      </div>
+    );
+  }
+  
+  export default App;
---
+++ index.js
@@ -0,0 +1,9 @@
+import App from "./App";
+
+const App = ()=>{
+   return (
+       <div>My React App with CDN</div>
+   )
+}
+  
+ReactDOM.render(<App />, document.querySelector('#root'));
