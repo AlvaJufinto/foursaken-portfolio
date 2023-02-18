4f49067f66024a678c7637cd2c53f94ff01acbdb
Alva Jufinto <yeahbruh199@gmail.com>
2023-02-18 22:35:39 +0700

    Change: header dan jumbotron

--- index.js
+++ index.js
@@ -6,18 +6,18 @@ const App = () => (
     <HashRouter>
       <nav className="flex justify-center font-semibold text-white mb-[-3rem]">
         <ul className="flex w-[1270px] max-w-[1270px] justify-between ml-10 mr-10 pt-3 font-sora bg-transparent">
-
           <div className="nav-logo flex">
             <img src="./img/arrow-navbar.png" className="max-w-[22px] max-h-[20px] relative top-[7.79px] "/>
             <li><Link to="/"><h1 className="ml-2 text-[24px]">Foursaken</h1></Link></li>
           </div>
 
-          <div className="flex justify-end space-x-10 text-[16px] mt-2">
-              <li><Link to="/work">Work</Link></li>
-              <li><Link to="/about">About</Link></li>
-              <li><Link to="/article">Article</Link></li>
-              <li><Link to="/contact">Contacts</Link></li>
-          </div>
+          {/* <div className="flex justify-end space-x-10 text-[16px] mt-2">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/work">Work</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/article">Article</Link></li>
+            <li><Link to="/contact">Contacts</Link></li>
+          </div> */}
 
         </ul>
       </nav>
@@ -35,45 +35,53 @@ const Home = () => {
         {
             name: 'Zaidan',
             image: './img/zaidan-transparent.png',
-            position: '0.2px'
+            position: '0px',
+            role: 'Team Leader',
         },
         {
             name: 'Alva',
             image: './img/alva-transparent.png',
-            position: '0.2px'
+            position: '0px',
+            role: 'Front-End Developer'
         },
         {
             name: 'Syafiq',
             image: './img/syafiq-transparent.png',
-            position: '0px'
+            position: '0px',
+            role: 'UI/UX Designer'
         },
         {
             name: 'Fajar',
             image: './img/pajar-transparent.png',
-            position: '0px'
+            position: '0px',
+            role: 'Back-End Developer',
         },
     ]
 
    return (
         <div id="header">
-          <div className="flex flex-col h-fit bg-[#98B7DE] z-[-2]  mb-[12rem] items-center justify-evenly gap-[10px]">   
+          <div className="flex flex-col h-[90vh] bg-[#C8B5FD] z-[-2] mb-[12rem] items-center justify-evenly gap-[10px]">   
             <h1 className="font-bold text-[26px] text-white mb-[4rem] mt-[8rem]">Creative Code House</h1>
-            <div className='flex flex-row'>
-              {
+            <div className='flex flex-row justify-between w-[100%] max-w-[1270px] h-[100%] overflow-hidden bg-[#000000]'>
+              { 
                   people.map((person) => (
-                      <div className={`relative top-[${person.position}] max-w-fit transition grayscale-[100%] hover:grayscale-0 hover:scale-125  hover:-translate-y-[3.5rem] `}>
+                      <div className={`relative top-[${person.position}] w-[300px] transition grayscale-[100%] hover:grayscale-0 hover:scale-125  translate-y-[12vw]`}>
+                          <div className="absolute right-[0px] top-[-15%]">
+                            <p>Hi👋, My name is {person.name}</p>
+                            <p>I'm a <span className="text-[#9747FF]">{person.role}</span></p>
+                          </div>
                           <img src={person.image} />
                       </div>
                   ))
               }            
               </div>
           </div>
-            <div class="marquee-w ">
-              <div class="marquee">
+            <div className="marquee-w">
+              <div className="marquee font-bebas">
                   <span>Foursaken ･ Foursaken ･ Foursaken ･ </span>
               </div>
-              <div class="marquee marquee2">
-                  <span>Foursaken ･ Foursaken ･ Foursaken ･ </span>
+              <div className="marquee marquee2 font-bebas">
+                  <span className="font-bebas">Foursaken ･ Foursaken ･ Foursaken ･ </span>
               </div>
             </div>
         </div>
