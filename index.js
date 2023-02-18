3624a35bc54ee08e602186b168c1e4957a7446be
jareee <fajarmulyos@gmail.com>
2023-02-18 08:01:00 +0700

    created navbar

--- index.js
+++ index.js
@@ -2,61 +2,71 @@ const { useState } = React;
 
 const { Link, Route, HashRouter} = ReactRouterDOM;
 
-// const Home = () => {
-//     const [index, setIndex] = useState(0);
-
-//     const people = [
-//         {
-//             name: 'Zaidan',
-//             color: '#D48C70'
-//         },
-//         {
-//             name: 'Alva',
-//             color: '#F4B9B8'
-//         },
-//         {
-//             name: 'Fajar',
-//             color: '#EBBBC3'
-//         },
-//         {
-//             name: 'Syafiq',
-//             color: '#E98980'
-//         },
-//     ]
-
-//    return (
-//         <div className="h-[100vh] bg-[blue] flex items-center justify-evenly gap-[10px]">   
-//             {
-//                 people.map((person, i) => (
-//                     <div className={`bg-[${person.color}] h-[500px] w-[100px] opacity-50 transition hover:opacity-100 hover:scale-125 hover:mx-[120px]`}>
-//                         <h3>{person.name}</h3>
-//                     </div>
-//                 ))
-//             }
-//         </div>
-//     );
-// }
-
 const App = () => (
     <HashRouter>
-      <nav className="flex justify-center ">
-        <ul className="flex w-[1270px] max-w-[1270px] justify-between ml-10 mr-[34px] mt-3 font-sora  bg-transparent">
-          <li><Link to="/">Foursaken</Link></li>
-          <div className="flex justify-end space-x-10">
-              <li className="justify-self-end"><Link to="/">Home</Link></li>
-              <li><Link to="/login">Login</Link></li>
-              <li><Link to="/register">Register</Link></li>
+      <nav className="flex justify-center font-semibold text-white">
+        <ul className="flex w-[1270px] max-w-[1270px] justify-between ml-10 mr-10 pt-3 font-sora bg-[#98B7DE]">
+
+          <div className="nav-logo flex">
+            <img src="./img/arrow-navbar.png" className="max-w-[22px] max-h-[20px] relative top-[7.79px] "/>
+            <li><Link to="/"><h1 className="ml-2 text-[24px]">Foursaken</h1></Link></li>
           </div>
+
+          <div className="flex justify-end space-x-10 text-[16px] mt-2">
+              <li><Link to="/work">Work</Link></li>
+              <li><Link to="/about">About</Link></li>
+              <li><Link to="/article">Article</Link></li>
+              <li><Link to="/contact">Contacts</Link></li>
+          </div>
+
         </ul>
       </nav>
 
       <Route path="/" exact component={Home} />
-      <Route path="/login" component={Login} />
+      <Route path="/about" component={Login} />
       <Route path="/register" component={Register} />
     </HashRouter>
   )
 
-  const Home = () => <h1>Home</h1>
+const Home = () => {
+    const [index, setIndex] = useState(0);
+
+    const people = [
+        {
+            name: 'Zaidan',
+            color: '#D48C70'
+        },
+        {
+            name: 'Alva',
+            color: '#F4B9B8'
+        },
+        {
+            name: 'Fajar',
+            color: '#EBBBC3'
+        },
+        {
+            name: 'Syafiq',
+            color: '#E98980'
+        },
+    ]
+
+   return (
+        <div className="container mx-auto px-4 h-[100vh] bg-[#98B7DE] relative bottom-[2.99rem] z-[-2] flex items-center justify-evenly gap-[10px]">   
+            {
+                people.map((person, i) => (
+                    <div className={`bg-[${person.color}] h-[500px] w-[100px] opacity-50 transition hover:opacity-100 hover:scale-125 hover:mx-[120px]`}>
+                        <h3>{person.name}</h3>
+                    </div>
+                ))
+            }
+        </div>
+    );
+}
+
+
+
+  
+  
   const Login = () => <h1>Login</h1>
   const Register = () => <h1>Register</h1>
   
