7d0c80cda5e3b1f42c69ceca086ea7e7b2d69725
Alva Jufinto <yeahbruh199@gmail.com>
2023-02-24 22:50:50 +0700

    update: syafiq medium article, navbar and footer standarization

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ window.onload = function() {
     setTimeout(function() {
       document.getElementById("preloader").style.display = "none";
       document.getElementById("content").style.display = "block";
-    }, 4500); 
+    }, 3500); 
   };
